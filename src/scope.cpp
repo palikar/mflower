810d50c9e4a9b7eed7c562ea1eab993856d43787
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-10-28 12:43:04 +0100

    Modifying the scope slighly

--- src/scope.cpp
+++ src/scope.cpp
@@ -1,65 +1,66 @@
-#include <iostream>
+// #include <iostream>
 
-// #include "tensor.hpp"
+// #include "scope.hpp"
 
-
+// namespace mf
+// {
 // Scope::Scope()
 // {
   
 // }
 
-// double* Scope::get_variable_value(const std::string& name) const
-// {
-//   if (this->variables.count(name)) {
-//     return this->variables.at(name);
-//   }
-//   std::cout << "No variable named " << name << "\n";
-//   exit(1);
-//   return 0;
-// }
+// // double* Scope::get_variable_value(const std::string& name) const
+// // {
+// //   if (this->variables.count(name)) {
+// //     return this->variables.at(name);
+// //   }
+// //   std::cout << "No variable named " << name << "\n";
+// //   exit(1);
+// //   return 0;
+// // }
 
-// Variable* Scope::new_variable(const std::string& name, double default_value)
-// {
-//   Variable* new_var = new Variable(name, Shape());
-//   new_var->data_size = 1;
-//   this->variables[name] = (double*) new(double);
-//   *this->variables[name] = default_value;
-//   return new_var;
-// }
+// // Variable* Scope::new_variable(const std::string& name, double default_value)
+// // {
+// //   Variable* new_var = new Variable(name, Shape());
+// //   new_var->data_size = 1;
+// //   this->variables[name] = (double*) new(double);
+// //   *this->variables[name] = default_value;
+// //   return new_var;
+// // }
 
 
-// Variable* Scope::new_variable(const std::string& name, std::vector<double> default_value)
-// {
-//   Variable* new_var = new Variable(name, Shape(default_value.size()));
-//   new_var->data_size = default_value.size();
-//   this->variables[name] = (double*) new double[sizeof(double)*default_value.size()]();
+// // Variable* Scope::new_variable(const std::string& name, std::vector<double> default_value)
+// // {
+// //   Variable* new_var = new Variable(name, Shape(default_value.size()));
+// //   new_var->data_size = default_value.size();
+// //   this->variables[name] = (double*) new double[sizeof(double)*default_value.size()]();
 
-//   size_t index = 0;
+// //   size_t index = 0;
 
-//   for (auto& val : default_value)
-//     this->variables[name][index++] = val;
+// //   for (auto& val : default_value)
+// //     this->variables[name][index++] = val;
   
-//   return new_var;
-// }
+// //   return new_var;
+// // }
 
 
-// Variable* Scope::new_variable(const std::string& name, Shape shape)
-// {
-//   Variable* new_var = new Variable(name,shape);
+// // Variable* Scope::new_variable(const std::string& name, Shape shape)
+// // {
+// //   Variable* new_var = new Variable(name,shape);
 
-//   int num = 1;
-//   if (shape.x >= 1)
-//     num *= shape.x;
-//   if (shape.y >= 1)
-//     num *= shape.x;
-//   if (shape.z >= 1)
-//     num *= shape.x;
+// //   int num = 1;
+// //   if (shape.x >= 1)
+// //     num *= shape.x;
+// //   if (shape.y >= 1)
+// //     num *= shape.x;
+// //   if (shape.z >= 1)
+// //     num *= shape.x;
 
-//   new_var->data_size = num;
-//   this->variables[name] = (double*) new(double);
-//   this->variables[name] = new double[num]();
-//   return new_var;
-// }
+// //   new_var->data_size = num;
+// //   this->variables[name] = (double*) new(double);
+// //   this->variables[name] = new double[num]();
+// //   return new_var;
+// // }
 
 
   
@@ -71,3 +72,4 @@
 
 
 
+// }
--- src/scope.hpp
+++ src/scope.hpp
@@ -1,21 +1,42 @@
+#pragma once
+
 #include <string>
 #include <unordered_map>
 
+#include "tensor.hpp"
 
 
-// class Scope{
-//   public:
-//     Scope();
-//     ~Scope();
+namespace mf
+{
 
-//     double* get_variable_value(const std::string& name) const;
-//     Variable* new_variable(const std::string& name, double default_value);
-//     Variable* new_variable(const std::string& name, std::vector<double> default_value);
-//     Variable* new_variable(const std::string& name, Shape shape) const;
+// class Variable;
+// template<typename T>
+// class DataBlock;
+
+
+class Scope
+{
+  public:
+    Scope()
+    {};
+
+    ~Scope()
+    {};
+
+    DataBlock<double> get_variable_value(const std::string& name) const
+    {
+        return m_variables.at(name);
+    }
+
+    void put_variable(const std::string& name, DataBlock<double> val)
+    {
+        m_variables.insert({name, val});
+    }
     
-//   private:
-
-//     std::unordered_map<std::string, double*> variables;
-
     
-// };
+  private:
+    
+    std::unordered_map<std::string, DataBlock<double>> m_variables;
+};
+
+}
