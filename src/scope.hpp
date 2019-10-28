ea1e887839f7864d88d441ae747a2ba5456675a2
Arnaudov, Stanislav Tomov <stanislav.arnaudov@iosb.fraunhofer.de>
2019-10-28 17:51:36 +0100

    Starting work on the evaluation context

--- src/scope.hpp
+++ src/scope.hpp
@@ -14,29 +14,38 @@ namespace mf
 // class DataBlock;
 
 
-class Scope
-{
+	class Scope
+	{
   public:
     Scope()
-    {};
+			{};
 
     ~Scope()
-    {};
+			{};
 
     DataBlock<double> get_variable_value(const std::string& name) const
-    {
+			{
         return m_variables.at(name);
-    }
+			}
 
     void put_variable(const std::string& name, DataBlock<double> val)
-    {
+			{
         m_variables.insert({name, val});
-    }
+			}
+
+		void push(){};
+		void pop(){};
+
+	
+		void set(const std::string& name, DataBlock<double> val)
+			{
+				m_variables.insert({name, val});
+			}
     
     
-  private:
+	private:
     
     std::unordered_map<std::string, DataBlock<double>> m_variables;
-};
+	};
 
 }
